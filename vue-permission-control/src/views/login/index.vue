fe27a6c8c5c9471ecbb2b8d24ecef31330b12aa0
chencqy <chen.qy1996@hotmail.com>
2019-06-16 11:02:01 +0800

    dashboard

--- vue-permission-control/src/views/login/index.vue
+++ vue-permission-control/src/views/login/index.vue
@@ -120,11 +120,6 @@ export default {
     },
     githubHandleClick(thirdpart) {
       alert('ok')
-      // this.$store.commit('SET_AUTH_TYPE', thirdpart)
-      // const client_id = 'xxxxx'
-      // const redirect_uri = encodeURIComponent('xxx/redirect?redirect=' + window.location.origin + '/auth-redirect')
-      // const url = 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri
-      // openWindow(url, thirdpart, 540, 540)
     }
   }
 }
