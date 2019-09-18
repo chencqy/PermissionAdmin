cda9bf3cc68036d13085a24c3f8d448cd75e4056
chencqy <chen.qy1996@hotmail.com>
2019-09-18 22:00:06 +0800

    eureka - admin_user

--- vtg/admin-user/src/main/java/com/chen/admin/user/AdminUserApplication.java
+++ vtg/admin-user/src/main/java/com/chen/admin/user/AdminUserApplication.java
@@ -1,11 +1,13 @@
 package com.chen.admin.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@MapperScan("com.chen.admin.user.mapper")
 public class AdminUserApplication {
 
     public static void main(String[] args) {
